127a0526774e8c4043c562e443747cc8f4b83edf
jaime-ac <jaimeantonioca97@gmail.com>
2025-02-12 22:41:06 -0300

    update da interface da home...

--- Aula 1/Gabarito 1/src/components/Main.jsx
+++ Aula 1/Gabarito 1/src/components/Main.jsx
@@ -1,14 +1,423 @@
 import './Main.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 
 function Main() {
   return (
     <div className='main'>
 
-        <div className='main__text'>
+      <div className="item-list">
+
+        <div className='item-list__header'>
             <h2>Artistas populares</h2>
 
-            <a className="main__link" href="/">Mostrar tudo</a>
+            <a className="item-list__link" href="/">Mostrar tudo</a>
         </div>
+
+        <div className="item-list__container">
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+              </div>
+
+              <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+              <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div>
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+              </div>
+
+              <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+              <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div>
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+              </div>
+
+              <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+              <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div>
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+              </div>
+
+              <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+              <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div>
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+              </div>
+
+              <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+              <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div>
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+              </div>
+
+              <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+              <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div>  
+
+        </div>
+
+      </div>
+
+      <div className="item-list">
+
+        <div className='item-list__header'>
+            <h2>Músicas populares</h2>
+
+            <a className="item-list__link" href="/">Mostrar tudo</a>
+        </div>
+
+        <div className="item-list__container">
+
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+          <div className="single-item">
+
+            <div className="single-item__div-image-button">
+                <div className="single-item__div-image">
+                  <img src="./src/assets/database/klash-brg.jpg" alt="Imagem do Cesar MC" className="single-item__image" />
+                </div>
+
+                <FontAwesomeIcon icon={faCirclePlay} className='single-item__icon' />
+            </div>
+
+            <div className="single-item__texts">
+
+              <div className="single-item-2lines">
+                <p className='single-item__title'>Klash BRG</p>
+              </div>
+
+                <p className='single-item__type'>Artista</p>
+
+            </div>
+
+          </div> 
+
+        </div>
+
+      </div>
       
     </div>
   )
